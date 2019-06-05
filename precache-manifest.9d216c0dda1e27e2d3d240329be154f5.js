3b259e326032f6d98c393491e8c35b6a8ef97bc0
Xiyang Liu <yippeecai@gmail.com>
2019-06-04 21:29:02 -0700

    Updates

---
+++ precache-manifest.9d216c0dda1e27e2d3d240329be154f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6e8addf1d9db33f8d3d64cee6dae45",
+    "url": "/dag-app/index.html"
+  },
+  {
+    "revision": "2ff74e772afb53cc4418",
+    "url": "/dag-app/static/js/2.727ddca6.chunk.js"
+  },
+  {
+    "revision": "64758771809c7e35a65b",
+    "url": "/dag-app/static/js/main.386096d0.chunk.js"
+  },
+  {
+    "revision": "a81fbd075fa2478ab6e9",
+    "url": "/dag-app/static/js/runtime~main.767b3239.js"
+  }
+]);
